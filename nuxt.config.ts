707f2315195e7d53b70c065a3c37a3df84be4f5b
Ryoji Yoshida <ryoji.ysd@gmail.com>
2024-07-13 09:34:52 +0900

    Add layout support and build the header of the app

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["@nuxt/ui", "@nuxtjs/supabase"],
-  supabase: {
-    redirect: true,
-  },
+  modules: ["@nuxt/ui"],
   runtimeConfig: {
     public: {
       baseUrl: process.env.BASE_URL ?? "http://localhost:3000",
