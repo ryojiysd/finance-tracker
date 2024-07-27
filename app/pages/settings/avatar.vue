46195ace8950f1869c3007c47c0f399e17c2d110
Ryoji Yoshida <ryoji.ysd@gmail.com>
2024-07-27 14:09:18 +0900

    Uploading files (avatar image): Actual upload to storage and access policies

--- app/pages/settings/avatar.vue
+++ app/pages/settings/avatar.vue
@@ -63,10 +63,26 @@ const saveAvatar = async () => {
   try {
     uploading.value = true;
     // 1. Grab the current avatar URL
+    const currentAvatarUrl = user.value.user_metadata?.avatar_url;
+
     // 2. Upload the image to avatars bucket
+    const { error } = await supabase.storage
+      .from("avatars")
+      .upload(fileName, file);
+
+    if (error) throw error;
+
     // 3. Update the user metadata with the avatar URL
+    await supabase.auth.updateUser({
+      data: {
+        avatar_url: fileName,
+      },
+    });
+
     // 4. (OPTIONALLY) remove the old avatar file
+
     // 5. Reset the file input
+    fileInput.value.input.value = null;
 
     toastSuccess({
       title: "Avatar uploaded",
