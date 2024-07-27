6e4709ec226c9fa4b513a40160e13aa17dc22c0c
Ryoji Yoshida <ryoji.ysd@gmail.com>
2024-07-27 11:00:25 +0900

    Refactor: Reusable app toast notification

--- app/components/transaction-modal.vue
+++ app/components/transaction-modal.vue
@@ -118,7 +118,7 @@ const schema = z.intersection(
 const form = ref();
 const isLoading = ref(false);
 const supabase = useSupabaseClient();
-const toast = useToast();
+const { toastSuccess, toastError } = useAppToast();
 
 const save = async () => {
   if (form.value.errors.length) return;
@@ -130,9 +130,8 @@ const save = async () => {
       .from("transactions")
       .upsert({ ...state.value });
     if (!error) {
-      toast.add({
+      toastSuccess({
         title: "Transaction saved",
-        icon: "i-heroicons-check-circle",
       });
       isOpen.value = false;
       emit("saved");
@@ -140,11 +139,9 @@ const save = async () => {
     }
     throw error;
   } catch (error) {
-    toast.add({
+    toastError({
       title: "Transaction not saved",
       description: error.message,
-      icon: "i-heroicons-exclamation-circle",
-      color: "red",
     });
   } finally {
     isLoading.value = false;
--- app/components/transaction.vue
+++ app/components/transaction.vue
@@ -50,7 +50,7 @@ const iconColor = computed(() =>
 const { currency } = useCurrency(props.transaction.amount);
 
 const isLoading = ref(false);
-const toast = useToast();
+const { toastSuccess, toastError } = useAppToast();
 const supabase = useSupabaseClient();
 
 const deleteTransaction = async () => {
@@ -58,17 +58,13 @@ const deleteTransaction = async () => {
 
   try {
     await supabase.from("transactions").delete().eq("id", props.transaction.id);
-    toast.add({
+    toastSuccess({
       title: "Transaction deleted",
-      icon: "i-heroicons-check-circle",
-      color: "green",
     });
     emit("deleted", props.transaction.id);
   } catch (error) {
-    toast.add({
-      title: "Transaction deleted",
-      icon: "i-heroicons-exclamation-circle",
-      color: "red",
+    toastError({
+      title: "Transaction was not deleted",
     });
   } finally {
     isLoading.value = false;
