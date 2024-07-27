88c875ebe15d10a14a3904b49b8555269e048148
Ryoji Yoshida <ryoji.ysd@gmail.com>
2024-07-27 12:04:24 +0900

    User Profile: Nested routes, vertical navigation and settings page

--- app/components/app-header.vue
+++ app/components/app-header.vue
@@ -48,7 +48,7 @@ const items = [
     {
       label: "Settings",
       icon: "i-heroicons-cog-8-tooth",
-      onClick: () => console.log("Link to settings in the future"),
+      click: () => navigateTo("/settings/profile"),
     },
     {
       label: "Sign out",
